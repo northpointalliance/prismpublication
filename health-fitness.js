560e9602f94cd4fc62ed0fe7535ea91b1aef5f36
northpointalliance <dan72ros@gmail.com>
2026-06-22 20:15:07 +0300

    Add DemoChat component for displaying niche

--- health-fitness.js
+++ health-fitness.js
@@ -1,1 +1,9 @@
+import React from 'react';
 
+export default function DemoChat({ niche }) {
+  return (
+    <div style={{ padding: '24px', textAlign: 'center' }}>
+      <p>Demo chat for: {niche}</p>
+    </div>
+  );
+}
