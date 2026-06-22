f9a9037f7c084bce8bc0bc1e000977e9cf2f3f8f
northpointalliance <dan72ros@gmail.com>
2026-06-22 20:15:46 +0300

    Add DemoChat component to wellness.js

--- wellness.js
+++ wellness.js
@@ -1,1 +1,10 @@
 
+import React from 'react';
+
+export default function DemoChat({ niche }) {
+  return (
+    <div style={{ padding: '24px', textAlign: 'center' }}>
+      <p>Demo chat for: {niche}</p>
+    </div>
+  );
+}
